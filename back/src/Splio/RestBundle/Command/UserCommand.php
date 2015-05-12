9d47603f6bb69379371049d4b032bd8f103e01e0
antho <aminier@splio.fr>
2015-05-12 16:48:41 +0200

    Add command to create user

---
+++ back/src/Splio/RestBundle/Command/UserCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Splio\RestBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Splio\WatchBundle\Entity\User;
+
+class UserCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('rest:userCreate')
+            ->setDescription('Create user')
+            ->addArgument(
+                'email',
+                InputArgument::REQUIRED,
+                'Email du user?'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $email = $input->getArgument('email');
+
+        $user = new User();
+
+        $user->setEmail('aminier@splio.fr');
+        $user->setCreatedAt(new \DateTime(null, new \DateTimeZone('UTC')));
+        $user->setEncryptedPassword('1234');
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        $output->writeln($user->getId());
+    }
+}
