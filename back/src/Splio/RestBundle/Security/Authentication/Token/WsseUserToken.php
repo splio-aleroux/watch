6fde867de9ecf9de37a5061d34b08c57169a9fb5
antho <aminier@splio.fr>
2015-05-12 13:44:01 +0200

    Add wsse support in rest bundle

---
+++ back/src/Splio/RestBundle/Security/Authentication/Token/WsseUserToken.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Acme\DemoBundle\Security\Authentication\Token;
+
+use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
+
+/**
+ *
+ */
+class WsseUserToken extends AbstractToken
+{
+    public $created;
+    public $digest;
+    public $nonce;
+
+    /**
+     *
+     */
+    public function __construct(array $roles = array())
+    {
+        parent::__construct($roles);
+
+        // Arrived here, user is authenticated
+        $this->setAuthenticated(true);
+    }
+
+    public function getCredentials()
+    {
+        return '';
+    }
+}
