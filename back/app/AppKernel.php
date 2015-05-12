7546b4597f58cdc2ea6c1d47eaeddf0d52fa51ec
Julio <jandri@splio.fr>
2015-05-12 11:40:33 +0200

    Delete authentication bundle

--- back/app/AppKernel.php
+++ back/app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Splio\RestBundle\SplioRestBundle(),
-            new Splio\AuthenticationBundle\SplioAuthenticationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
