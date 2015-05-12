4e5d18f260bd40ed176355f436f95684514d333b
Damien Bonnay <dbonnay@splio.fr>
2015-05-12 16:30:56 +0200

    Rename jsx to js. And add signin/signout route

---
+++ front/components/SignOut.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var SignOut = React.createClass({
+  render: function() {
+    return (
+        <div className="container">
+            Bye Nigga!
+        </div>
+    );
+  }
+});
+
+
+module.exports = SignOut;
--- front/routes.js
+++ front/routes.js
@@ -4,14 +4,14 @@
 
 var React = require('react');
 var Router = require('react-router');
-var App = require('./app.jsx');
-var CommentBox = require('./components/CommentBox.jsx');
 var Route = Router.Route;
 var DefaultRoute = Router.DefaultRoute;
+
 var Routes = (
-    <Route path="/" handler={App}>
-        <Route name="coucou" handler={CommentBox} />
-        <DefaultRoute handler={CommentBox} />
+    <Route path="/" handler={require('./app.jsx')}>
+        <DefaultRoute handler={require('./components/CommentBox.js')} />
+        <Route name="sign-in" path="/signin" handler={require('./components/SignIn.js')} />
+        <Route name="sign-out" path="/signout" handler={require('./components/SignOut.js')} />
     </Route>
 );
 
