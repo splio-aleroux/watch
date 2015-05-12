f8436c423b9dc611934048c19a534acfdddff355
splio-cbonachera <cbonachera@splio.fr>
2015-05-12 11:12:23 +0200

    add auhtentication bundlze

--- back/app/AppKernel.php
+++ back/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Splio\RestBundle\SplioRestBundle(),
+            new Splio\AuthenticationBundle\SplioAuthenticationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
