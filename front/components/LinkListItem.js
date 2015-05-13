4e802a9a15ec24b22a6e328a323ab197a898eb6c
Damien Bonnay <dbonnay@splio.fr>
2015-05-13 17:07:50 +0200

    Add new link template

--- front/components/LinkListItem.js
+++ front/components/LinkListItem.js
@@ -3,10 +3,20 @@ var React = require('react');
 var LinkListItem = React.createClass({
   render: function() {
     return (
-        <li>
-            url: <a href={this.props.url}>{this.props.url}</a><br />
-            created: {this.props.createAt}
-        </li>
+        <div>
+            <div className="col-md-12">
+                <div className="col-md-2">
+                    <span className="glyphicon glyphicon-heart-empty">42 reco</span>
+                </div>
+                <div className="col-md-8">
+                    <a href={this.props.url}>{this.props.url}</a>
+                </div>
+            </div>
+            <div className="col-md-12">
+                <div className="col-md-2"><span className="glyphicon glyphicon-certificate">12 click</span></div>
+                <div className="col-md-8">{this.props.createAt} - @penis - #tag1 #tag2 #tag3 #tag4</div>
+            </div>
+        </div>
     );
   }
 });
